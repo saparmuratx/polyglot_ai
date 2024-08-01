ccdba216f51f61d96f6555ea53a144900507538c
Saparmurat <saparmurat@protonmail.com>
2024-08-01 12:13:15 +0600

    add translate route

--- src/main.py
+++ src/main.py
@@ -3,9 +3,27 @@ from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 
+from sqlalchemy.orm import Session
+
+from . import models, schemas, crud, utils
+
+from .database import engine, get_db
+
+from .settings import settings
+
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 templates = Jinja2Templates(directory="src/templates")
+
+models.Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
@@ -26,3 +44,21 @@ def foo(request: Request):
 @app.get("/bar", response_class=HTMLResponse)
 def bar(request: Request):
     return templates.TemplateResponse("bar.html", {"request": request})
+
+
+@app.post("/translate", response_model=schemas.TaskResponse)
+async def translate(
+    request: schemas.TranslationRequest,
+    background_tasks: BackgroundTasks,
+    db: Session = Depends(get_db),
+):
+    task = crud.create_translation_task(db, request.text, request.languages)
+
+    background_tasks.add_task(utils.perform_translation())
+
+    return task
+
+
+@app.get("/translate/{task_id}", response_model=schemas.TranslationStatus)
+async def translate_get(task_id: int, db: Session = Depends(get_db)):
+    return crud.get_translation_task(db, task_id)
